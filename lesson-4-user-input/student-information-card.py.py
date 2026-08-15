9875c7bbc6e452df5f4e8fe64cf4c1087ab5dc93
laraib <laraibjay2@gmail.com>
2026-08-16 02:24:09 +0500

    Add Python learning projects

---
+++ lesson-4-user-input/student-information-card.py.py
@@ -0,0 +1,15 @@
+
+student_name = "Laraib Javed"
+student_age = 20
+student_city = "Bahawalnagar"
+student_university = "IUB"
+student_program = "Artificial Intelligence"
+
+print("---------- STUDENT INFORMATION CARD ----------")
+print("Student name:", student_name )
+print("Student age:", student_age )
+print("Student city:", student_city )
+print("Student university:", student_university )
+print("Student program:", student_program )
+
+print("-------------------------")
---
+++ lesson-5-operators/simple-calculator.py.py
@@ -0,0 +1,17 @@
+# Your task is to build a calculator that:
+
+# Asks the user for two numbers.
+# Performs all seven operations.
+# Displays the results clearly
+
+
+first_num = int(input("Enter first number:"))
+second_num= int(input("Enter second number:"))
+
+print("addition:",first_num + second_num )
+print("subtract:",first_num - second_num )
+print("multiplicaton:",first_num * second_num )
+print("division:",first_num / second_num )
+print("remainder:",first_num % second_num )
+print("power:",first_num ** second_num )
+print("floor division:",first_num // second_num )
